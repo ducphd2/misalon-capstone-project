aeb0dcdb08c174412d1f12bf21aa8165548e5801
Duc Pham Huynh <phamhuynhduc0212@gmail.com>
2023-04-23 22:52:38 +0700

    Fix scripts (#8)
    Co-authored-by: ducph <ducphd2@gmail.com>

--- scripts/pm2-start.sh
+++ scripts/pm2-start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-concurrently --kill-others "nest start @app/api-gateway" "nest start @app/user-service" "nest start @app/merchant-service"
+pm2 start dist/apps/api-gateway/main.js --name gateway
+pm2 start dist/apps/user-service/main.js --name user-service
+pm2 start dist/apps/merchant-service/main.js --name merchant-service
