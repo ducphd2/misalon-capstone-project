163b22bf39ca956300af40dcac675e006f809ba8
ducph <ducphd2@gmail.com>
2023-05-25 10:43:10 +0700

    Fix update user

---
+++ apps/api-gateway/src/dtos/user/create-user.dto.ts
@@ -0,0 +1,83 @@
+import { CommonProto } from '@libs/grpc-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength, IsString, IsEnum, IsInt, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @Transform(({ value }: TransformFnParams) => value?.trim().toLowerCase())
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MaxLength(30)
+  @MinLength(5)
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsOptional()
+  @IsInt()
+  merchantId?: number;
+
+  @IsOptional()
+  @IsInt()
+  branchId?: number;
+
+  @IsOptional()
+  @IsEnum(CommonProto.EUserStatus)
+  status: CommonProto.EUserStatus;
+
+  @IsEnum(CommonProto.EUserGender)
+  @IsOptional()
+  gender: CommonProto.EUserGender;
+
+  @IsOptional()
+  @IsEnum(CommonProto.ECustomerLevel)
+  level?: CommonProto.ECustomerLevel;
+
+  @IsOptional()
+  @IsString()
+  phoneNumber: string;
+
+  @IsInt()
+  @IsOptional()
+  dobDay: number;
+
+  @IsInt()
+  @IsOptional()
+  dobMonth: number;
+
+  @IsInt()
+  @IsOptional()
+  dobYear: number;
+
+  @IsString()
+  @IsOptional()
+  job?: string;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @IsInt()
+  @IsOptional()
+  cityCode?: number;
+
+  @IsInt()
+  @IsOptional()
+  districtCode?: number;
+
+  @IsInt()
+  @IsOptional()
+  wardCode?: number;
+}
+
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['password', 'email'])) {}
