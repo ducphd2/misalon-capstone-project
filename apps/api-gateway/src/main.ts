895866a7f88fbeb7f389d75e1052ee8678be81f4
Duc Pham Huynh <phamhuynhduc0212@gmail.com>
2023-05-29 09:02:57 +0700

    Bugfix/fix cors local (#73)
    * Fix cors local
    
    * Fix cors local
    
    ---------
    
    Co-authored-by: ducph <ducphd2@gmail.com>

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const configService = app.get<ConfigService>(ConfigService);
 
   const cors = {
-    origin: ['http://localhost:4000'],
+    origin: '*',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
