d37dda7e001bff3417305354afce2c35f5f97f39
Duc Pham Huynh <phamhuynhduc0212@gmail.com>
2023-06-04 18:19:04 +0700

    Impl nearest service (#91)
    Co-authored-by: ducph <ducphd2@gmail.com>

--- apps/api-gateway/src/dtos/merchant/service/get-service.dto.ts
+++ apps/api-gateway/src/dtos/merchant/service/get-service.dto.ts
@@ -1,3 +1,18 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 import { BaseQueryDto } from '../../base';
 
 export class GetServiceDto extends BaseQueryDto {}
+
+export class GetNearestServiceDto extends GetServiceDto {
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsNotEmpty()
+  latitude: number;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsNotEmpty()
+  longitude: number;
+}
