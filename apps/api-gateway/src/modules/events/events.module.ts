5624767a574d31e40e5ddff6da364073a6275f0b
ducph <ducphd2@gmail.com>
2023-06-08 21:04:45 +0700

    Fix chat notification

--- apps/api-gateway/src/modules/events/events.module.ts
+++ apps/api-gateway/src/modules/events/events.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SecretsModule, SecretsService } from '@libs/modules';
+import { BullQueueProvider, SecretsModule, SecretsService } from '@libs/modules';
+import { BullModule } from '@nestjs/bull';
+import { EBullQueue } from '@libs/core';
 
 import { MessagesModule } from '../messages/messages.module';
 
@@ -10,7 +12,24 @@ import { FirebaseModule } from '@/notification-service/firebase/firebase.module'
 import { FirebaseService } from '@/notification-service/firebase/firebase.service';
 
 @Module({
-  imports: [MessagesModule, FirebaseModule, UserCommonModule, SecretsModule],
-  providers: [EventsGateway, FirebaseService, SecretsService],
+  imports: [
+    MessagesModule,
+    FirebaseModule,
+    UserCommonModule,
+    SecretsModule,
+    BullModule.registerQueue({
+      name: EBullQueue.NOTIFICATION_QUEUE,
+    }),
+    BullModule.registerQueue({
+      name: EBullQueue.GATEWAY_QUEUE,
+    }),
+    BullModule.registerQueue({
+      name: EBullQueue.BOOKING_QUEUE,
+    }),
+    BullModule.registerQueue({
+      name: EBullQueue.USER_QUEUE,
+    }),
+  ],
+  providers: [EventsGateway, FirebaseService, SecretsService, BullQueueProvider],
 })
 export class EventsModule {}
